54f2805a72c7e2deaaf4a5c8dd93591c3fa1ec41
Mike Bannister <notimpossiblemike@gmail.com>
2020-03-22 01:58:16 -0400

    Improve everything

--- index.js
+++ index.js
@@ -1,86 +1,104 @@
+#!/usr/bin/env node
+
 const shell = require('shelljs')
 const { tmpdir } = require('os')
 const { ulid } = require('ulid')
-const { join, resolve, dirname } = require('path')
-const { existsSync } = require('fs')
-const range = require('lodash/range')
+const { join, dirname } = require('path')
+const { existsSync, readdirSync } = require('fs')
 const findSyncableGroups = require('./findSyncableGroups')
 
-const isVerbose = process.argv.includes('--verbose')
+const minimist = require('minimist')
+
+const argv = minimist(process.argv.slice(2))
+
+const {
+  _: [command],
+  's3-bucket': s3Bucket,
+  's3-prefix': s3Prefix
+} = argv
 
 const usage = message => {
-  console.error('Usage: nextjs-export-to-s3 <mode> <bucket>')
+  console.error('Usage: nextjs-export-to-s3 <args>')
   if (message) {
     console.error(message)
   }
   process.exit(1)
 }
 
-const preview = async bucket => {
+const upload = (s3Bucket, s3Prefix) => {
   if (!existsSync('dist')) {
     throw new Error('`./dist` does not exist')
   }
 
   const workDir = join(tmpdir(), ulid().toLowerCase())
-  const groups = findSyncableGroups('dist')
+
+  shell.mkdir('-p', workDir)
+  shell.cp('-r', 'dist', workDir)
+  shell.mv(join(workDir, 'dist', '_next'), workDir)
+  shell.mkdir('-p', join(workDir, 'pages-0'))
+  shell.mv(join(workDir, 'dist/*'), join(workDir, 'pages-0'))
+  shell.rm('-r', join(workDir, 'dist'))
+
+  const groups = findSyncableGroups(join(workDir, 'pages-0'))
   groups.forEach((group, groupIndex) =>
     group.forEach(path => {
-      const from = join(resolve('dist'), `${path}.html`)
-      const to = join(workDir, `group-${groupIndex.toString()}`, path)
+      const from = join(workDir, 'pages-0', `${path}.html`)
+      const to = join(workDir, `pages-${(groupIndex + 1).toString()}`, path)
       shell.mkdir('-p', dirname(to))
       shell.cp(from, to)
     })
   )
 
-  const { code } = shell.exec(
-    `
-      aws s3 sync dist ${bucket} \
-        --cache-control max-age=0,no-cache \
-        --acl public-read
-    `,
-    { quiet: !isVerbose }
-  )
+  const bucket = `s3://${s3Bucket}${s3Prefix ? `/${s3Prefix}` : ''}`
+
+  shell.cd(workDir)
+
+  const { code } = shell.exec(`
+    aws s3 sync ${join(workDir, '_next')} ${bucket} \
+      --cache-control immutable,max-age=100000000,public \
+      --acl public-read
+  `)
   if (code) {
     throw new Error('Could not sync `./dist` to S3')
   }
 
-  shell.cd(workDir)
-  for (const groupIndex of range(groups.length)) {
-    const groupPath = join(workDir, `group-${groupIndex.toString()}`)
-    const { code } = shell.exec(
-      `
-        aws s3 sync ${groupPath} ${bucket} \
+  readdirSync(workDir)
+    .filter(path => path.includes('pages-'))
+    .forEach(pagePath => {
+      const pagesPath = join(workDir, pagePath)
+      const { code } = shell.exec(`
+        aws s3 sync ${pagesPath} ${bucket} \
           --cache-control max-age=0,no-cache \
           --content-type text/html \
           --acl public-read
-      `,
-      { quiet: !isVerbose }
-    )
-    if (code) {
-      throw new Error(`Could not sync group \`${groupIndex}\` to S3`)
-    }
-  }
+      `)
+      if (code) {
+        throw new Error(`Could not sync page group \`${pagesPath}\` to S3`)
+      }
+    })
 }
 
-const production = () => {
-  console.error('Error: production not yet implemented')
+const deploy = (s3Bucket, s3Prefix) => {
+  console.error('Error: deploy not yet implemented')
   process.exit(1)
 }
 
-const commands = { preview, production }
+const commands = { upload, deploy }
 
 const commandNames = Object.keys(commands)
-const command = process.argv[2]
-const bucket = process.argv[3]
 
-if (!bucket) {
-  usage('Error: bucket is required')
+if (!s3Bucket) {
+  usage('Error: s3 bucket is required')
+}
+
+if (!s3Prefix) {
+  usage('Error: s3 prefix is required')
 }
 
 if (!commandNames.includes(command)) {
-  usage('Error: command name must be `preview` or `production`')
+  usage('Error: command name must be `upload` or `deploy`')
 }
 
 const handler = commands[command]
 
-handler(bucket).catch(error => usage(`Error: ${error.message}`))
+handler(s3Bucket, s3Prefix).catch(error => usage(`Error: ${error.message}`))
--- readme.md
+++ readme.md
@@ -1,17 +1,5 @@
 # NextJS export to S3
 
-This CLI prepares a NextJS exported app for S3 deployment. The main responsibility is to add an extension-less copy of every html page, e.g. `foo.html` becomes `foo` to S3 alongside the files that are created during `next export`. This is not possible on the filesystem (because you cannot have a file and directory with the same name) but is possible and necessary on s3 so a secondary goal of is to sort the tree into as few s3-syncable groups as possible given the full tree and then upload each group with `aws s3 sync` rather than uploading each separately just to avoid writing to disk.
-
-## Distributions
-
-### Preview
-
-A preview distribution is a standalone release that is usually temporary. There are no caching benefits and CloudFront is not required.
-
-### Production
-
-A production distribution is a standalone release that supports releases, rollbacks, and caching with CloudFront.
-
 ## Installation
 
 ```
@@ -20,24 +8,12 @@ yarn global add nextjs-export-to-s3
 
 ## Usage
 
-To deploy a standalone preview build
+Upload a build
 
 ```
-nextjs-export-to-s3 preview s3://my-bucket/foo
-```
-
-To deploy a production build
-
-```
-nextjs-export-to-s3 production s3://my-bucket/foo
-```
-
-To release a production build
-
-```
-nextjs-export-to-s3 production s3://my-bucket/foo --current
+nextjs-export-to-s3 --s3-bucket my-bucket --s3-prefix foo
 ```
 
 ## Inspiration
 
-Everything in this repo is based on the following gist: https://gist.github.com/rbalicki2/30e8ee5fb5bc2018923a06c5ea5e3ea5
+Based on [How we incorporate next and cloudfront](https://gist.github.com/rbalicki2/30e8ee5fb5bc2018923a06c5ea5e3ea5).
